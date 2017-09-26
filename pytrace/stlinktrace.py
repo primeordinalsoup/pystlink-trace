88e1b4767890b15e010424a8e33769bf0343f3b5
Peter Kraak <pkraak@dynamiccontrols.com>
2017-09-26 16:41:03 +1300

    simplified setting up ITM/TPIU registers.

--- pytrace/stlinktrace.py
+++ pytrace/stlinktrace.py
@@ -26,47 +26,38 @@ class StlinkTrace():
     def readSWO(self):
         num = self._stlink.get_trace_buffered_count()
         if ( num > 0 ):
-            # Demeter?  This is obviously wrong but pyswd not our toy.
-            return self._stlink._com.read_swo()
-            #use  property getter;  return self._stlink.com.read_swo()
+            return self._stlink.com.read_swo()
         else:
             return None
 
-    def _w(self, addr, data):
-        print("write_mem32 {:08x} <= {:04x}".format(addr, data));
-        self._stlink.write_mem32(addr, list((data).to_bytes(4, byteorder='little')))
-        # PKPK todo:  OR try set_mem32(addr, INT), writes one 4 byte number?
-        print("last sts: {}".format(self._stlink.get_last_rw_status()))
-
     def _setupSWOTracing(self, xtal_hz):
         # captured via tshark from openocd with tpiu config
-        self._w(0xe000edfc, 0x01000000)
-        self._w(0xe0040004, 0x00000001)
-        ## self._w(0xe0040010, 0x00000023)   # 72/35+1 = 2
-        self._w(0xe0040010, int(xtal_hz/2000000 - 0.5))  # -1 + 0.5 for rounding => -0.5
-        self._w(0xe00400f0, 0x00000002)
-        self._w(0xe0040304, 0x00000100)
-        self._w(0xe0042004, 0x00000327)
-        self._w(0xe0000fb0, 0xc5acce55)
-        self._w(0xe0000e80, 0x00010009)
-        self._w(0xe0000e00, 0xffffffff)
-        self._w(0xe0000e04, 0x00000000)
-        self._w(0xe0000e08, 0x00000000)
-        self._w(0xe0000e0c, 0x00000000)
-        self._w(0xe0000e10, 0x00000000)
-        self._w(0xe0000e14, 0x00000000)
-        self._w(0xe0000e18, 0x00000000)
-        self._w(0xe0000e1c, 0x00000000)
-        self._w(0xe0002008, 0x00000000)
-        self._w(0xe000200c, 0x00000000)
-        self._w(0xe0002010, 0x00000000)
-        self._w(0xe0002014, 0x00000000)
-        self._w(0xe0002018, 0x00000000)
-        self._w(0xe000201c, 0x00000000)
-        self._w(0xe0002020, 0x00000000)
-        self._w(0xe0002024, 0x00000000)
-        self._w(0xe0001028, 0x00000000)
-        self._w(0xe0001038, 0x00000000)
-        self._w(0xe0001048, 0x00000000)
-        self._w(0xe0001058, 0x00000000)
-        self._w(0xe0042004, 0x00000327)
+        self._stlink.set_mem32(0xe000edfc, 0x01000000)
+        self._stlink.set_mem32(0xe0040004, 0x00000001)
+        self._stlink.set_mem32(0xe0040010, int(xtal_hz/2000000 - 0.5))  # -1 + 0.5 for rounding => -0.5
+        self._stlink.set_mem32(0xe00400f0, 0x00000002)
+        self._stlink.set_mem32(0xe0040304, 0x00000100)
+        self._stlink.set_mem32(0xe0042004, 0x00000327)
+        self._stlink.set_mem32(0xe0000fb0, 0xc5acce55)
+        self._stlink.set_mem32(0xe0000e80, 0x00010009)
+        self._stlink.set_mem32(0xe0000e00, 0xffffffff)
+        self._stlink.set_mem32(0xe0000e04, 0x00000000)
+        self._stlink.set_mem32(0xe0000e08, 0x00000000)
+        self._stlink.set_mem32(0xe0000e0c, 0x00000000)
+        self._stlink.set_mem32(0xe0000e10, 0x00000000)
+        self._stlink.set_mem32(0xe0000e14, 0x00000000)
+        self._stlink.set_mem32(0xe0000e18, 0x00000000)
+        self._stlink.set_mem32(0xe0000e1c, 0x00000000)
+        self._stlink.set_mem32(0xe0002008, 0x00000000)
+        self._stlink.set_mem32(0xe000200c, 0x00000000)
+        self._stlink.set_mem32(0xe0002010, 0x00000000)
+        self._stlink.set_mem32(0xe0002014, 0x00000000)
+        self._stlink.set_mem32(0xe0002018, 0x00000000)
+        self._stlink.set_mem32(0xe000201c, 0x00000000)
+        self._stlink.set_mem32(0xe0002020, 0x00000000)
+        self._stlink.set_mem32(0xe0002024, 0x00000000)
+        self._stlink.set_mem32(0xe0001028, 0x00000000)
+        self._stlink.set_mem32(0xe0001038, 0x00000000)
+        self._stlink.set_mem32(0xe0001048, 0x00000000)
+        self._stlink.set_mem32(0xe0001058, 0x00000000)
+        self._stlink.set_mem32(0xe0042004, 0x00000327)
